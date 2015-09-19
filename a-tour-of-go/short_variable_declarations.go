47f4efd657788f250268e165956c4fc15ffdc160
Moza USANE <mozamimy@quellencode.org>
2015-09-19 22:58:51 +0900

    Add some tutrials.

---
+++ a-tour-of-go/short_variable_declarations.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i, j int = 1, 2
+	k := 3
+	ruby, erlang, csharp := true, false, "no!"
+
+	fmt.Println(i, j, k, ruby, erlang, csharp)
+}
---
+++ a-tour-of-go/variables.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var i int
+var ruby, erlang, csharp bool
+
+func main() {
+	fmt.Println(i, ruby, erlang, csharp)
+}
